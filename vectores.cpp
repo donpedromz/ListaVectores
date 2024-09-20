f3a19b60b2c7bbd648e98fb36a5931c1e3693e33
donpedromz <153339097+donpedromz@users.noreply.github.com>
2024-09-20 15:29:55 -0500

    Ejercicio Repaso V1

---
+++ vectores.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void mostrarListado(vector<int> list){
+    vector<int>::iterator it;
+    int posicion = 1;
+    for(it = list.begin(); it < list.end(); it++){
+        cout<<"POSICION: "<<posicion<<" CLAVE DE TURNO: "<<*it<<"\n"; 
+        posicion++;      
+    }
+}
+void darTurno(vector<int>& list, int *turnos)
+{
+    int prioritario, position, nuevoTurno = *turnos;
+    do
+    {
+        cout << "USTED ES PRIORITARIO? \n 1.SI \n 2.NO \n";
+        cin >> prioritario;
+
+    } while (prioritario < 1 || prioritario > 2);
+    if(prioritario == 1){
+        cout<<"ACTUALMENTE HAY: "<<list.size()<<"\n INDIQUE POR FAVOR UNA POSICION: ";
+        cin>>position;      
+    }else{
+        list.push_back(*turnos);
+        cout<<"SU CLAVE DE TURNO ES: "<<*turnos<<"\n";
+        *turnos = nuevoTurno += 1;
+        cout<<"TURNO AGREGADO CORRECTAMENTE \n";
+    }
+}
+int main()
+{
+    bool salidaSistema = false;
+    vector<int> turnos;
+    int numeroTurnos = 1; // Clave de turnos autoincrementable
+    do
+    {
+        int opc;
+        cout << "---SISTEMA DE ATENCION AL CLIENTE--- \n";
+        cout << "1. DAR TURNO \n";
+        cout << "2. ATENDER PERSONA \n";
+        cout << "3. CANCELAR TURNO \n";
+        cout << "4. MOSTRAR LISTADO TURNOS \n";
+        cout << "5. SALIR DEL SISTEMA \n";
+        cin >> opc;
+        switch (opc)
+        {
+        case 1:
+            cout << "---DAR TURNO---- \n";
+            darTurno(turnos, &numeroTurnos)     ;
+            break;
+        case 2:
+            cout << "---ATENDER PERSONA---- \n";
+            break;
+        case 4:
+            cout<<"---MOSTRAR LISTADO TURNOS--- \n";
+            mostrarListado(turnos);
+            break;
+        }
+    } while (!salidaSistema);
+}
